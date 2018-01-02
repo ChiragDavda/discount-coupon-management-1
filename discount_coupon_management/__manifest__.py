4af291b3ec062b1de7f48b1cc78407d42553ffcd
ChiragDavda <chiragdavda007@gmail.com>
2018-01-02 14:05:16 +0530

    [FIX]Solve problem of relationship

--- discount_coupon_management/__manifest__.py
+++ discount_coupon_management/__manifest__.py
@@ -3,11 +3,9 @@
 	'name':'Discount Coupon Management',
 	'version':'1',
 	'summary':'Manage Discount Coupon',
-	'description':"""This is description for Discount Coupon Management!!! """,
-	'depends':[
-	'sale','sale_management'],
+	'description':"""This is description for Discount Coupon Management!!! """,	
 	'data':[
-		'wizard/change_coupon_detail.xml',
+		#'wizard/change_coupon_detail.xml',
 		'views/make_discount_view.xml',
 		'views/make_company_view.xml',
 		'views/new_bill.xml',
